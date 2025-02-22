33118420a9009fe9f185b875e9a722249d309075
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-02-22 12:53:53 +0530

    bug: fix multi language support

--- code-execution/entrypoint.sh
+++ code-execution/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Code-execution started"
-
 SOURCE_CODE="$SOURCE_CODE"
 EXPECTED_OUTPUT="$EXPECTED_OUTPUT"
 LANGUAGE="$LANGUAGE"    
@@ -12,18 +10,18 @@ if [ $LANGUAGE = "javascript" ]; then
     node /app/source_code.js > /app/output.txt 2>&1
 
 elif [ $LANGUAGE = "java" ]; then
-    echo "$SOURCE_CODE" > /app/Code.java
+    echo "$SOURCE_CODE" > /app/source_code.java
     echo "$EXPECTED_OUTPUT" > /app/expected_output.txt
-    javac /app/Code.java 2> /app/compile_errors.txt
+    javac /app/source_code.java 2> /app/compile_errors.txt
     
     if [ $? -ne 0 ]; then
         echo "ExpectedOutput: $EXPECTED_OUTPUT"
-        echo "StdOut:"
-        cat /app/compile_errors.txt
+        ERROR=$(cat /app/compile_errors.txt | tr -d '[:space:]')
+        echo "StdOut: $ERROR"
         echo "Result: Wrong Answer"
         exit 1
     fi
-    java -cp /app Code > /app/output.txt 2>&1
+    java /app/source_code.java > /app/output.txt 2>&1
 
 else
     echo "Unsupported language: $LANGUAGE"
@@ -42,5 +40,3 @@ if [ "$OUTPUT" = "$EXPECTED_OUTPUT" ]; then
 else
     echo "Result: Wrong Answer"
 fi
-
-echo "Code-execution completed"
