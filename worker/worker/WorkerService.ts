7afba16a336ce49c588316496ec529f222f4e55f
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-03-19 11:14:25 +0530

    feat: add ecs container logic

--- worker/worker/WorkerService.ts
+++ worker/worker/WorkerService.ts
@@ -1,16 +1,30 @@
 import { SubmissionStatus } from "../../shared/entity/Submission";
 import { SubmissionService } from "../../shared/service/SubmissionService";
-import { Worker } from "bullmq";
+import { tryCatch, Worker } from "bullmq";
 import Docker from "dockerode";
 import { redisConfig } from "../../shared/config/redis.config";
 import { redisClient } from "worker/config/redis.config";
+import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
 
 class WorkerService {
   private docker: Docker;
   private readonly EXECUTION_TIMEOUT = 10000; // 10 seconds timeout
 
+  private ecs: ECSClient;
+  private clusterName: string = process.env.CLUSTER_NAME || ""; // Replace with your ECS Cluster name
+  private taskDefinition: string = process.env.TASK_DEFINITION || ""; // Replace with your ECS Task Definition
+  private subnetIds: string[] = process.env.SUBNET_IDS?.split(",") || [""]; // Replace with your actual subnets
+  private securityGroupIds = process.env.SECURITYGROUPD_IDS?.split(",") || [""]; // Replace with your actual security groups
+
   constructor(private submissionService: SubmissionService) {
     this.docker = new Docker();
+    this.ecs = new ECSClient({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || " ",
+      },
+      region: process.env.AWS_REGION || "",
+    });
   }
 
   private isErrorOutput(output: string): boolean {
@@ -86,6 +100,59 @@ class WorkerService {
     }
   }
 
+  private async runEcsTask(
+    submissionId: string,
+    questionId: string,
+    sourceCode: string,
+    language: string,
+    expectedOutput: string
+  ): Promise<string> {
+    try {
+      const command = new RunTaskCommand({
+        cluster: this.clusterName,
+        launchType: "EC2", // Use "FARGATE" if using AWS Fargate
+        taskDefinition: this.taskDefinition,
+        overrides: {
+          containerOverrides: [
+            {
+              name: "worker-container",
+              environment: [
+                { name: "SUBMISSION_ID", value: submissionId },
+                { name: "QUESTION_ID", value: questionId },
+                { name: "SOURCE_CODE", value: sourceCode },
+                { name: "LANGUAGE", value: language },
+                { name: "EXPECTED_OUTPUT", value: expectedOutput },
+              ],
+            },
+          ],
+        },
+        networkConfiguration: {
+          awsvpcConfiguration: {
+            subnets: this.subnetIds,
+            securityGroups: this.securityGroupIds,
+            assignPublicIp: "ENABLED",
+          },
+        },
+        count: 1, // Runs a single ECS task per submission
+      });
+
+      const response = await this.ecs.send(command);
+      if (!response?.tasks) {
+        throw new Error("No tasks returned from ECS RunTask");
+      }
+      if (!response?.tasks[0]?.containers) {
+        throw new Error("No containers returned from ECS RunTask");
+      }
+      console.log(
+        "ECS Task started:",
+        response?.tasks[0]?.containers[0].containerArn
+      );
+      return response?.tasks[0]?.containers[0].containerArn || "";
+    } catch (error) {
+      console.error("Failed to start ECS Task:", error);
+    }
+  }
+
   async startWorker() {
     const worker = new Worker(
       "submission",
@@ -98,18 +165,25 @@ class WorkerService {
           if (!submission) {
             throw new Error("Submission not found");
           }
-          container = await this.docker.createContainer({
-            Image: "code-executioner-multlanguage:latest",
-            Tty: true,
-            Env: [
-              `SOURCE_CODE=${submission.submission.getCode()}`,
-              `EXPECTED_OUTPUT=${submission.submission.getExpectedOutput()}`,
-              `LANGUAGE=${submission.submission.getLanguage()}`,
-            ],
-          });
+          // container = await this.docker.createContainer({
+          //   Image: "code-executioner-multlanguage:latest",
+          //   Tty: true,
+          //   Env: [
+          //     `SOURCE_CODE=${submission.submission.getCode()}`,
+          //     `EXPECTED_OUTPUT=${submission.submission.getExpectedOutput()}`,
+          //     `LANGUAGE=${submission.submission.getLanguage()}`,
+          //   ],
+          // });
 
-          await container.start();
-          console.log(`Container started: ${container.id}`);
+          // await container.start();
+
+          const container = await this.runEcsTask(
+            id,
+            questionId,
+            submission.submission.getCode(),
+            submission.submission.getLanguage(),
+            submission.submission.getExpectedOutput()
+          );
 
           // Set up timeout handler
           const timeoutPromise: Promise<string> = new Promise((_, reject) => {
@@ -131,7 +205,7 @@ class WorkerService {
               timeoutPromise,
             ]);
 
-            await container.wait();
+            // await container.wait();
 
             const responseArray = output.split("\n").map((line) => line.trim());
 
